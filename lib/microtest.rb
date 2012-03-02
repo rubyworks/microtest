2d579051e86ce573bb93eeb8a6b9f36a79682b07
7rans <transfire@gmail.com>
2012-03-02 12:22:48 -0500

    Fix when setup and teardown are run.

--- lib/microtest.rb
+++ lib/microtest.rb
@@ -1,10 +1,14 @@
 module MicroTest
 
   #
+  # Output naturalized test names instead of just method names.
+  # 
   def self.natural_names
     @natural_names ||= nil
   end
 
+  #
+  # Set flag to output naturalized test names instead of just method names.
   #
   def self.natural_names=(boolean)
     @natural_names = !!boolean
@@ -18,39 +22,57 @@ module MicroTest
   #
   class TestCase < World
 
+    #
+    # When the TestCase class is inherited, a new instance is 
+    # automatically created.
+    #
     def self.inherited(subclass)
       subclass.new
     end
 
+    #
+    # Create a new test and add it the the $TEST_SUITE global variable.
     #
     def self.new(*a,&b)
       $TEST_SUITE << super(*a,&b)
     end
 
     #
+    # Returns name of testcase class.
+    #
     def to_s
       self.class.name
     end
 
     #
+    # Wrap test case run.
+    #
+    # @todo: Support setup-all and teardown-all in future ?
+    #
     def call(&cont)
-      setup
+      #setup_all
       cont.call
-      teardown
+      #teardown_all
     end
 
+    #
+    # Iterate over each test.
     #
     def each
       methods.each do |m|
         next unless m.to_s.start_with?('test_')
-        yield(TestMethod.new(method(m)))
+        yield(TestMethod.new(self, method(m)))
       end
     end
 
     #
+    # No-op for test setup routine.
+    #
     def setup
     end
 
+    #
+    # No-op for test teardown routine.
     #
     def teardown
     end
@@ -58,13 +80,21 @@ module MicroTest
   end
 
   #
+  # Encapsualtes test method for execution by RubyTest. Mainly
+  # this separate encapsulation allows the test description to
+  # be something other than just the method name, e.g. if the
+  # `MicroTest.natural_names` flag is set to true.
+  #
   class TestMethod
-    def initialize(method)
-      @method = method
+    def initialize(testcase, method)
+      @testcase = testcase
+      @method   = method
     end
 
     def call
+      @testcase.setup
       @method.call
+      @testcase.teardown
     end
 
     def to_s
--- test/test_case.rb
+++ test/test_case.rb
@@ -6,8 +6,15 @@ class ExampleTest < MicroTest::TestCase
   end
 
   #
-  def test_alpha_is_one
+  def test_alpha
     assert_equal(1, @a)
+    @a = 2
+  end
+
+  #
+  def test_alpha_again
+    assert_equal(1, @a)
+    @a = 2
   end
 
 end
